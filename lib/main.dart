6bb384a706bfd0aa7848f8c58214d367b1dc0095
avivv <>
2022-07-29 00:12:52 +0300

    V = 0.2 firebase to android + to ios (but missing Podfile, platform :ios, '9.0' to '10.0')

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 
-import 'package:aviv_lerning_full_power/common/app_bar.dart';
+// import 'package:aviv_lerning_full_power/common/app_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'reoven_page.dart';
-import 'common/my_app_bar.dart';
+// import 'common/my_app_bar.dart';
 import 'log_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -15,9 +21,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: logInPage()
-      home: reovenPage()
+      home: logInPage()
+      // home: reovenPage()
     );
   }
 }
 
+//TODO Podfile, platform :ios, '9.0'
